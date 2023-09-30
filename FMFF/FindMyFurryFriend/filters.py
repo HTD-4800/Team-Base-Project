25368f2ead94fdb7c42991d68d341fbd85580ec9
unknown <19tnguyen60@gmail.com>
2023-09-30 03:29:22 -0700

    Your commit message here

--- FMFF/FindMyFurryFriend/filters.py
+++ FMFF/FindMyFurryFriend/filters.py
@@ -1,8 +1,12 @@
-# filters.py
 import django_filters
 from .models import LostPet
 
 class LostPetFilter(django_filters.FilterSet):
+
+    # Define additional filter fields
+    # name = django_filters.CharFilter(lookup_expr='icontains')  # Case-insensitive partial match
+    # location = django_filters.CharFilter(lookup_expr='iexact')  # Case-insensitive exact match
+
     class Meta:
         model = LostPet
-        fields = ['species']  # Add the fields that are part of the LostPet model
+        fields = ['species', 'name']  # Add more fields as needed
